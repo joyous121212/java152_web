963c8606321cd5890b670d606b2744e1dd94988c
joyous121212 <joyous95@gmail.com>
2024-07-22 17:53:52 +0900

    240722

--- springboot2/src/main/java/com/itwill/springboot2/domain/Department.java
+++ springboot2/src/main/java/com/itwill/springboot2/domain/Department.java
@@ -1,7 +1,12 @@
 package com.itwill.springboot2.domain;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,10 +17,17 @@ import lombok.ToString;
 @Entity
 @Table(name = "DEPT")
 public class Department {
-	@Id
-	private Integer deptno;
-	
-	private String dname;
-	
-	private String loc;
+    @Id
+    @Column(name = "DEPTNO")
+    private Integer id;
+    
+    private String dname;
+    
+    @Column(name = "LOC")
+    private String location;
+    
+    @ToString.Exclude
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "department")
+    // mappedBy: Employee 엔터티에서 @OneToMany 애너테이션이 설정된 필드 이름.
+    private List<Employee> employees;
 }
--- springboot2/src/main/java/com/itwill/springboot2/domain/Employee.java
+++ springboot2/src/main/java/com/itwill/springboot2/domain/Employee.java
@@ -1,10 +1,13 @@
 package com.itwill.springboot2.domain;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -15,26 +18,31 @@ import lombok.ToString;
 
 @NoArgsConstructor @Getter @ToString @EqualsAndHashCode
 @Entity // 데이터베이스 테이블과 매핑하는 자바 객체
-@Table(name = "EMP") // 클래스 이름과 실제 테이블 이름이 다를 때.
+@Table(name = "EMP") // 클래스 이름과 실제 테이블 이름이 다를 때. 
 public class Employee {
-	@Id // Primary Key
-	@Column(name = "EMPNO") // 필드 이름과 실제 컬럼 이름이 다를 때
-	private Integer id;
-	
-	private String ename;
-	
-	private String job;
-	
-	@Column(name = "MGR")
-	private Integer manager;
-	
-	private LocalDateTime hiredate;
-	
-	@Column(name = "SAL")
-	private Double salary;
-	
-	@Column(name = "COMM")
-	private Double commission;
-	
-	private Integer deptno;
+    @Id // Primary Key
+    @Column(name = "EMPNO") // 필드 이름과 실제 컬럼 이름이 다를 때.
+    private Integer id;
+    
+    private String ename;
+    
+    private String job;
+    
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MGR")
+    private Employee manager;
+    
+    private LocalDate hiredate;
+    
+    @Column(name = "SAL")
+    private Double salary;
+    
+    @Column(name = "COMM")
+    private Double commission;
+    
+    @ToString.Exclude // toString 메서드의 출력 문자열에서 제외.
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DEPTNO") // EMP 테이블에서 DEPT 테이블과 join하는 컬럼 이름.
+    private Department department;
 }
