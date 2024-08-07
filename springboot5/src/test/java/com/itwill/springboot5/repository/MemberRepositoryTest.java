0f91b4dfba34837407cce770d512352acacb5efc
joyous121212 <joyous95@gmail.com>
2024-08-07 16:26:25 +0900

    240807

--- springboot5/src/test/java/com/itwill/springboot5/repository/MemberRepositoryTest.java
+++ springboot5/src/test/java/com/itwill/springboot5/repository/MemberRepositoryTest.java
@@ -2,10 +2,13 @@ package com.itwill.springboot5.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.itwill.springboot5.domain.Member;
 import com.itwill.springboot5.domain.MemberRole;
@@ -30,19 +33,28 @@ public class MemberRepositoryTest {
 		log.info(passwordEncoder.toString());
 	}
 	
-	@Test
+//	@Test
 	public void testSave() {
 		// 엔터티 객체를 DB members 테이블에 저장.
 		
 		Member m = Member.builder()
-				.username("test1")
-				.password(passwordEncoder.encode("1111"))
-				.email("test1@itwill.com")
+				.username("test2")
+				.password(passwordEncoder.encode("2222"))
+				.email("test2@itwill.com")
 				.build();
 		m.addRole(MemberRole.USER);
-		log.info("save 호출 전 = {}", m);
+		m.addRole(MemberRole.ADMIN);
+		log.info("save 호출 전 = {}, {}", m, m.getRoles());
 		
 		m = memberRepo.save(m);
-		log.info("save 호출 후 = {}", m);
+		//-> members 테이블, member_roles 테이블에 insert.
+		log.info("save 호출 후 = {}, {}", m, m.getRoles());
+	}
+	
+	@Test @Transactional
+	public void testFindAll() {
+		List<Member> list = memberRepo.findAll();
+		//-> members 테이블과 member_roles 테이블에서 정보를 취합.
+		list.forEach((member) -> log.info("{}, {}", member, member.getRoles()));
 	}
 }
